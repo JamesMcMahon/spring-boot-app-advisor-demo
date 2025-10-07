9ce89a63a267238386190b4f9143110f05f9a8f3
James F McMahon <JamesMcMahon@users.noreply.github.com>
2025-10-07 15:40:42 -0500

    [Auto] Upgrading spring-data-relational to 3.0.x, spring-security to 6.0.x, spring-boot to 3.0.x, spring-data-commons to 3.0.x, spring-framework to 6.0.x and micrometer to 1.10.x

--- src/main/java/sh/jfm/upgradeexample/SecurityConfig.java
+++ src/main/java/sh/jfm/upgradeexample/SecurityConfig.java
@@ -4,27 +4,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(sessions -> sessions.requireExplicitAuthenticationStrategy(false));
+        http.securityContext(sc -> sc.requireExplicitSave(false));
         http
-                .authorizeRequests()
-                .antMatchers("/message", "/db-messages").permitAll()
-                .antMatchers("/secure-message").hasRole("ADMIN")
+                .authorizeHttpRequests()
+                .requestMatchers(AntPathRequestMatcher.antMatcher("/message"), AntPathRequestMatcher.antMatcher("/db-messages")).permitAll()
+                .requestMatchers(AntPathRequestMatcher.antMatcher("/secure-message")).hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+        return http.build();
     }
 
     @Bean
